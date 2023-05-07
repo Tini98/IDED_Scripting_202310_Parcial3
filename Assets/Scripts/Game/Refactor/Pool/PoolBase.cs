0f6f591f8c50f8727743479618363f24fcf657c0
Tini Roldan <68084594+Tini98@users.noreply.github.com>
2023-05-06 23:36:54 -0500

    sufrir

--- Assets/Scripts/Game/Refactor/Pool/PoolBase.cs
+++ Assets/Scripts/Game/Refactor/Pool/PoolBase.cs
@@ -4,28 +4,21 @@ using UnityEngine;
 
 public abstract class PoolBase : MonoBehaviour, IPool
 {
-    // Atributo privado que indica la cantidad de objetos que se instanciarán en la pool
     [SerializeField]
     private int count = 0;
 
-    // Atributo privado que almacena el prefab que se instanciará para crear los objetos de la pool
     [SerializeField]
     private GameObject basePrefab;
 
-    // Lista que almacena las instancias creadas
     public List<GameObject> instances = new List<GameObject>();
 
-    // Método Start que se ejecuta al inicio del juego
     private void Start()
     {
-        // Llama al método "PopulatePool()" para crear las instancias de la pool
         PopulatePool();
     }
 
-    // Método que crea las instancias de la pool
     private void PopulatePool()
     {
-        // Ciclo for que crea "count" instancias de "basePrefab" y las agrega a la lista "instances"
         for (int i = 0; i < count; i++)
         {
             instances.Add(Instantiate(basePrefab, transform.position, Quaternion.identity));
@@ -33,22 +26,17 @@ public abstract class PoolBase : MonoBehaviour, IPool
         }
     }
 
-    // Método virtual que debe ser implementado por las clases que hereden de "PoolBase"
-    // Devuelve un GameObject de la pool que se encuentre inactivo
     public virtual GameObject GetOBJ()
     {
-        // Ciclo for que recorre la lista "instances" en busca de un objeto inactivo
         for (int i = 0; i < instances.Count; i++)
         {
             if (!instances[i].activeInHierarchy)
             {
-                // Si encuentra un objeto inactivo, lo activa, configura su comportamiento a través del método "SetUp()" y lo devuelve
                 instances[i].SetActive(true);
                 instances[i].GetComponent<Ipoolable>().SetUp(2);
                 return instances[i];
             }
         }
-        // Si no encuentra ningún objeto inactivo, devuelve "null"
         return null;
     }
 }
